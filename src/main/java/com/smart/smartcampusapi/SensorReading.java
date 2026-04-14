76d004a359657f288759f9240da25301058343e8
Stoyan Ivanov <ivanov.stoyan100@gmail.com>
2026-04-14 22:42:49 +0100

    Created the room.java, sensor.java and sensorReading.java

--- src/main/java/com/smart/smartcampusapi/SensorReading.java
+++ src/main/java/com/smart/smartcampusapi/SensorReading.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smart.smartcampusapi;
 
 /**
@@ -10,4 +6,40 @@ package com.smart.smartcampusapi;
  */
 public class SensorReading {
     
+    private String id;
+    private long timestamp;
+    private double value;
+    
+    public SensorReading() {
+    }
+    
+    public SensorReading(String id, long timestamp, double value) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public long getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    public double getValue() {
+        return value;
+    }
+    
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
