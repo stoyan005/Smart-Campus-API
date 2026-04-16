efcf9000c47ebb89307abbc447767a84a8de42d9
Stoyan Ivanov <ivanov.stoyan100@gmail.com>
2026-04-17 00:00:36 +0100

    Created the room.java, sensor.java and sensorReading.java

--- src/main/java/com/smart/smartcampusapi/Room.java
+++ src/main/java/com/smart/smartcampusapi/Room.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smart.smartcampusapi;
 
-/**
- *
- * @author Stoyan
- */
 import java.util.ArrayList;
 import java.util.List;
 
